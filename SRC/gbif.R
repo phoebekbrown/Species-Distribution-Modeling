1d0fd39d8d77eca74837da78706eb91cf5e8e08c
phoebekbrown <lc20-0156@lclark.edu>
2024-02-20 20:02:46 +0000

    finished data cleaning

--- SRC/gbif.R
+++ SRC/gbif.R
@@ -65,4 +65,14 @@ fData <- fData %>%
 fData <- fData %>%
   cc_sea(lon="decimalLongitude", lat="decimalLatitude")
 
+# remove duplicates
+fData <- fData %>%
+  distinct(decimalLongitude, decimalLongitude, speciesKey, datasetKey, .keep_all = TRUE)
 
+# one fell swoop:
+# cleanData <- d %>%
+#   filter(!is.na(decimalLatitude), !is.na(decimalLongitude)) %>%
+#   filter(countryCode %in% c("US", "CA", "MX")) %>%
+#   filter(!basisOfRecord %in% c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN")) %>%
+#   cc_sea(lon="decimalLongitude", lat="decimalLatitude") %>%
+#   distinct(decimalLongitude, decimalLongitude, speciesKey, datasetKey, .keep_all = TRUE)
